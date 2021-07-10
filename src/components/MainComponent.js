20389736f1265baf8a0f9ceacd3ce2453e4271d1
Amadeusz Sepko <a.sepko@gmail.com>
2021-07-09 22:07:01 -0400

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
 	//gets state from redux
@@ -17,6 +18,10 @@ const mapStateToProps = (state) => {
 		partners: state.partners,
 		promotions: state.promotions,
 	};
+};
+
+const mapDispatchToProps = {
+	addComment: (campsiteId, rating, author, text) => addComment(campsiteId, rating, author, text),
 };
 
 class Main extends Component {
@@ -36,6 +41,7 @@ class Main extends Component {
 				<CampsiteInfo
 					campsite={this.props.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
 					comments={this.props.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
+					addComment={this.props.addComment}
 				/>
 			);
 		};
@@ -57,5 +63,5 @@ class Main extends Component {
 	}
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 //withRouter function will allow react router to still work with changes to export
